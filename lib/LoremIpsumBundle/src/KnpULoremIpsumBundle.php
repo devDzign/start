13b47e6d513bbdd2f7bf0f1f6dea9184bec460f2
MCH3730 <Yasmina!!2008>
2018-07-19 17:59:20 +0200

    alias for configuration

--- lib/LoremIpsumBundle/src/KnpULoremIpsumBundle.php
+++ lib/LoremIpsumBundle/src/KnpULoremIpsumBundle.php
@@ -9,9 +9,19 @@
 namespace KnpU\LoremIpsumBundle;
 
 
+use KnpU\LoremIpsumBundle\DependencyInjection\KnpULoremIpsumExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KnpULoremIpsumBundle extends Bundle
 {
-
+    /**
+     * Overridden to allow for the custom extension alias.
+     */
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new KnpULoremIpsumExtension();
+        }
+        return $this->extension;
+    }
 }
