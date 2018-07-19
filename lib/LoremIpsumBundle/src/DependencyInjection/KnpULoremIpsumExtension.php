692cbb4879a733c17b9674e871f87e1c717d82ec
MCH3730 <Yasmina!!2008>
2018-07-19 17:36:24 +0200

    Auto-Adding Services

---
+++ lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MCH3730
+ * Date: 19/07/2018
+ * Time: 17:24
+ */
+
+namespace KnpU\LoremIpsumBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class KnpULoremIpsumExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ lib/LoremIpsumBundle/src/KnpULoremIpsumBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MCH3730
+ * Date: 19/07/2018
+ * Time: 17:22
+ */
+
+namespace KnpU\LoremIpsumBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KnpULoremIpsumBundle extends Bundle
+{
+
+}
